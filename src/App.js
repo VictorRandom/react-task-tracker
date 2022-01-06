9f145f7ec9136abfbb445c04bf339caf1b3413a8
Victor Miranda Balduino <victormbalduino@hotmail.com>
2022-01-06 20:37:47 -0300

    passando valores para o componente pai

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 function App() {
 
+  
+  const [tasks, setTasks] = useState([   
+    {
+        id: 1,
+        text: 'Consulta médica',
+        day:'5 de fevereiro as 14:30h',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Reunião escolar',
+        day:'6 de fevereiro as 13:30h',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Almoço no Shopping',
+        day:'5 de fevereiro as 12:30h',
+        reminder: false,
+    }
+])
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,6 @@
 import React, { useState } from 'react'
 
-function Tasks() {
-
-    const [tasks, setTasks] = useState([   
-        {
-            id: 1,
-            text: 'Consulta médica',
-            day:'5 de fevereiro as 14:30h',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Reunião escolar',
-            day:'6 de fevereiro as 13:30h',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Almoço no Shopping',
-            day:'5 de fevereiro as 12:30h',
-            reminder: false,
-        }
-    ])
+function Tasks({ tasks }) {
 
     return (
         
