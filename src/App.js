c4308d6e1ff93699de8beb75ef462c157547bb75
Victor Miranda Balduino <victormbalduino@hotmail.com>
2022-01-10 15:00:03 -0300

    fazer o formulario de adicionar uma nova task aparecer e desaparecer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -29,7 +30,7 @@ function App() {
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1;
     const newTask = { id, ...task };
-    setTasks([...tasks, newTask])
+    setTasks([...tasks, newTask]);
   };
 
   // Delete Task
@@ -48,8 +49,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask (!showAddTask)} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
